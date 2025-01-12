965aca0fc41cfc42a24ea414369929cac6b51b27
Muhammad Abdulrehman <muhammadabdulrehman513@gmail.com>
2025-01-11 19:40:16 -0500

    sup

--- Week 1/HW01.py
+++ Week 1/HW01.py
@@ -1,1 +1,6 @@
-hourly_wage=(float(input("What is your hourly wage")))
+hourly_wage=(float(input("What is your hourly wage?:")))
+work_hours=(int(input("How many hours have you worked in a week:")))
+work_year=(int(input("how many weeks in a year have you worked:")))
+
+income_week=hourly_wage* work_hours
+print (income_week)     
